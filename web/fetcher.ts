amarula/checks-jenkins

commit 3199568a52a35f8f818c7013167ffb4571d3a663

fetcher.ts: Add explain error plugin error fetch
Allow to read what was the reason of the failure for that
run.

Change-Id: I8a7ca102b033f9de105dab805a631a35e33c44eb
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

web/fetcher.ts
@@ -104,6 +104,11 @@ interface AnalysisReport {
   issues: AnalysisIssue[];
 }
 
+interface ErrorResponse {
+  _class: string;
+  explanation?: string;
+}
+
 export class ChecksFetcher implements ChecksProvider {
   private plugin: PluginApi;
 
@@ -166,6 +171,37 @@ export class ChecksFetcher implements ChecksProvider {
         continue;
       }
       const key: RequestKey = [jenkins.name, changeData.changeNumber, changeData.patchsetNumber, totalRuns]
+
+      for (const run of data.runs) {
+        if (run.status === RunStatus.COMPLETED) {
+          if (!run.results) {
+            run.results = [];
+          }
+          const errorResult: boolean = run.results.find((result: CheckResult) => result.category === Category.ERROR);
+          if (errorResult) {
+            const errorMessage = await this.explainBuildFailure(jenkins, changeData, run.statusLink);
+            if (errorMessage) {
+              const lines = errorMessage.split('\n');
+              const parsedSummary = lines[0].trim();
+              const detailedMessage = lines.slice(1).join('\n').trim();
+              const markdownMessage = detailedMessage
+                ? `\`\`\`text\n${detailedMessage}\n\`\`\``
+                : 'No additional details provided.';
+
+              if (!run.results) {
+                run.results = [];
+              }
+
+              run.results.push({
+                category: Category.ERROR,
+                summary: parsedSummary,
+                message: markdownMessage
+              });
+            }
+          }
+        }
+      }
+
       const cachedData: CheckRun[] = await cacheService.get(key);
       if (cachedData === null || cachedData === undefined || cachedData.length == 0) {
         let warningsData: CheckRun[] = [];
@@ -386,6 +422,26 @@ export class ChecksFetcher implements ChecksProvider {
     })
 
     return checkRuns;
+  }
+
+  async explainBuildFailure(jenkins: Config, changeData: ChangeData, statusLink: string) {
+    const errorResult = await (async () => {
+      try {
+        return await this.fetchFromJenkins(jenkins, changeData.repo, `${statusLink}error-explanation/api/json`, "GET");
+      } catch (e) {
+        return null;
+      }
+    })();
+
+    if (errorResult === null || (errorResult.ok != undefined && !errorResult.ok)) {
+      return null;
+    }
+
+    const errorInfo: ErrorResponse = await this.toJson(errorResult);
+    if (errorInfo === null) {
+      return null;
+    }
+    return errorInfo.explanation;
   }
 
   fetchConfig(changeData: ChangeData): Promise<Config[]> {

commit b03ff59aaac8b2158fcaed3392bca6d797ae76ed

fetcher.ts: Fix duplicated entry in check tab
Fixes: 3199568a52a3 ("fetcher.ts: Add explain error plugin error fetch")
Change-Id: Id5431479dd606277c37fa59c719bf34eca40aad0
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

web/fetcher.ts
@@ -183,7 +183,7 @@ export class ChecksFetcher implements ChecksProvider {
           if (!run.results) {
             run.results = [];
           }
-          const errorResult: boolean = run.results.find((result: CheckResult) => result.category === Category.ERROR);
+          const errorResult = run.results.find((result: CheckResult) => result.category === Category.ERROR);
           if (errorResult) {
             const errorMessage = await this.explainBuildFailure(jenkins, changeData, run.statusLink);
             if (errorMessage) {
@@ -198,11 +198,8 @@ export class ChecksFetcher implements ChecksProvider {
                 run.results = [];
               }
 
-              run.results.push({
-                category: Category.ERROR,
-                summary: parsedSummary,
-                message: markdownMessage
-              });
+              errorResult.summary = parsedSummary;
+              errorResult.message = markdownMessage;
             }
           }
         }